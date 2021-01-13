660d668268a4a3e3d3aa059158bfad85e17343d2
SanchezYoan <77174458+SanchezYoan@users.noreply.github.com>
2021-01-13 11:30:06 +0100

    Add files via upload

---
+++ post-install(readme).sh
@@ -0,0 +1,15 @@
+READ ME :
+
+Je dis à mon environnement os d'utiliser bash comme interpréteur de commande.
+
+La commande ECHO permet d'afficher une ligne un peu comme un commentaire.
+
+La commande sudo permet à l'utilisateur d'avoir la possibilité d'installer ou d'éxécuter
+les programmes en tant qu'administateur
+
+La commande apt-get permet d'installer ou de desinstaller un programme en provenance d'un
+
+Le format snap sert à permettre l'installation de nouvelles versions de logiciel sur linux
+tout en apportant au developpeur une facilitée de distribution, fiabilité et sécurité.
+
+Je demande au terminal d'installer discord et snap --
